d853e1d5d4c0a582f0eba11992fc2f4cb54230ad
Martin Ogden <martin.ogden@gmail.com>
2016-07-24 18:47:09 +0100

    add navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
+import { Navbar, NavItem, Icon } from 'react-materialize';
 
 import GameListContainer from './GameListContainer.jsx';
 
 
 const App = ({ store }) => (
-	<Provider store={ store }>
-		<GameListContainer />
-	</Provider>
+	<div>
+		<div className="navbar-fixed">
+			<Navbar brand='&nbsp;24h du Jeu' right>
+				<NavItem href='#'><Icon>search</Icon></NavItem>
+			</Navbar>
+		</div>
+
+		<Provider store={ store }>
+			<GameListContainer />
+		</Provider>
+	</div>
 );
 
 App.PropTypes = {
