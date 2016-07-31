309042c70661d21b8fa0c988e2cea6f7f8ca09ac
Martin Ogden <martin.ogden@gmail.com>
2016-07-31 18:57:21 +0100

    add tests for token reducer

--- client/tests/reducers/auth.js
+++ client/tests/reducers/auth.js
@@ -4,9 +4,10 @@ import { ActionTypes } from 'constants';
 import auth from 'reducers/auth';
 
 
+const ACCESS_TOKEN = 'TEST';
 const action = {
 	type: ActionTypes.AUTH_USER_SUCCESS,
-	payload: { access_token: 'TEST' },
+	payload: { access_token: ACCESS_TOKEN },
 };
 
 
@@ -22,6 +23,23 @@ describe('reducers::auth::isLoggedIn', () => {
 	it('should return false as default state', () => {
 		const state = auth({}, {});
 		assert.deepEqual(state['isLoggedIn'], false);
+	})
+
+})
+
+
+describe('reducers::auth::token', () => {
+
+	describe('on AUTH_USER_SUCCESS', () => {
+		it('should return provided access_token', () => {
+			const state = auth({}, action);
+			assert.deepEqual(state['token'], ACCESS_TOKEN);
+		})
+	})
+
+	it('should return null as default state', () => {
+		const state = auth({}, {});
+		assert.deepEqual(state['token'], null);
 	})
 
 })
