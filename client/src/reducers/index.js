37b2072dbb8267c56d54807a6137f9fe1d41b1b4
Martin Ogden <martin.ogden@gmail.com>
2016-08-01 00:01:45 +0100

    [NEEDS TESTS] add owned game and known game selectors

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,29 @@ import games, * as fromGames from './games';
 import auth from './auth';
 
 
+/**
+ * Utility to to find games that user knows or owns
+ *
+ * @param {String} by - (own|know)
+ * @param {Array} state
+ * @returns {Array} of game IDs
+ */
+const filterGameIDs = (by, state) => {
+	const userID = state.auth.userID;
+
+	if (!userID)
+		return [];
+
+	const isUserInList = (users, id) => (
+		users.filter(uid => uid === id).length > 0
+	);
+
+	return getGames(state)
+		.filter(game => isUserInList(game[by], userID))
+		.map(game => game.id);
+}
+
+
 export default combineReducers({
 	games,
 	auth,
@@ -11,3 +34,5 @@ export default combineReducers({
 
 // Selectors
 export const getGames = (state) => fromGames.getGames(state.games);
+export const getOwnedGameIDs = (state) => filterGameIDs('owners', state);
+export const getKnownGameIDs = (state) => filterGameIDs('knowers', state);
