8fab82de654ef8b84cbd5f0874b101c4ffd47d21
Céline Aussourd <celine.aussourd@gmail.com>
2016-09-04 21:05:47 +0100

    changing color

--- client/src/components/AddGame.js
+++ client/src/components/AddGame.js
@@ -3,7 +3,7 @@ import { Button } from 'react-materialize';
 
 const AddGame = () => (
 	<div className="fixed-action-btn" style={{bottom: '45px', right: '24px',}}>
-		<Button floating large className='teal lighten-1' waves='light' icon='add'/>
+		<Button floating large className='teal' waves='light' icon='add'/>
 	</div>
 );
 
