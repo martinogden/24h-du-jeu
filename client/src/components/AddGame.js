a3490cd482ba9d39824f72a281f31b465848800c
Céline Aussourd <celine.aussourd@gmail.com>
2016-09-07 20:56:32 +0100

    adding form for new game

--- client/src/components/AddGame.js
+++ client/src/components/AddGame.js
@@ -1,10 +1,50 @@
 import React, { PropTypes } from 'react';
-import { Button } from 'react-materialize';
+import { Button, Modal, Row, Input, Icon } from 'react-materialize';
+
+//TODO Add constraints. e.g. durée
+//TODO Get genre lists directly from DB? (type column)
+//TODO Use Range for durée??
 
 const AddGame = () => (
-	<div className="fixed-action-btn" style={{bottom: '45px', right: '24px',}}>
-		<Button floating large className='teal' waves='light' icon='add'/>
-	</div>
+	<div>
+		<Modal
+			//header='Nouveau jeu'
+			trigger={
+				<div className="fixed-action-btn" style={{bottom: '45px', right: '24px',}}>
+					<Button floating large className='teal' waves='light' icon='add'/>
+				</div>
+			}
+			actions={
+				<div>
+			      <Button modal="close" waves="light" className="teal">Ajouter</Button>
+			    </div>
+			}>
+				<Row>
+					<Input s={9} label="Titre"><Icon>mode_edit</Icon></Input>
+					<Input s={3} label="ID BoardGameGeek" defaultValue="" disabled />
+					<Input s={12} m={4} type='select' label="Genre">
+					    <option value="" disabled selected>Sélectionner le genre</option>
+					    <option value='1'>Ambiance</option>
+					    <option value='2'>Coopératif</option>
+					    <option value='3'>Enchères</option>
+					    <option value='4'>Enfants</option>
+					    <option value='5'>Gestion</option>
+					    <option value='6'>Logique</option>
+					    <option value='7'>Parcours</option>
+					    <option value='8'>Placement</option>
+					    <option value='9'>Stratégie</option>
+					</Input>
+					<Input s={6} m={2} label="Joueurs min." />
+					<Input s={6} m={2} label="Joueurs max." />
+					<Input s={6} m={2} label="Durée (mins)" />
+					<Input s={6} m={2} label="Age min." />
+					<div className="input-field col s12">
+						<textarea id="description" className="materialize-textarea"></textarea>
+	          			<label for="description">Description</label>
+          			</div>
+				</Row>
+		</Modal>
+    </div>
 );
 
 export default AddGame;
