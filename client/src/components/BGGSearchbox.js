e5c2c0efaa24faeeddc27e3ed1ed8a0f63ddc8de
Céline Aussourd <celine.aussourd@gmail.com>
2016-09-26 09:51:19 +0200

    styling the autocomplete box and make the suggestions disappear when click on element

--- client/src/components/BGGSearchbox.js
+++ client/src/components/BGGSearchbox.js
@@ -3,8 +3,35 @@ import { Input, Icon } from 'react-materialize';
 
 import BaseSearchbox from './BaseSearchbox';
 
+const styles = {  // TODO extract inline styles
+	autodropdown: {
+		width: '1005px',
+		position: 'absolute',
+		top: '60px', 
+		left: '11.25px', 
+		opacity: '1',
+		display: 'block',
+	},
+};
+
 
 class BGGSearchbox extends BaseSearchbox {
+
+	close() {
+		this.reset();
+	}
+
+	renderSuggestion(game) {
+
+		return (
+			<li
+				key={ game.id }
+				onClick={ this.close.bind(this) }
+			>
+				{ game.name }
+			</li>
+		);
+	}
 
 	renderAutocomplete() {
 		const games = this.props.autocomplete;
@@ -13,15 +40,16 @@ class BGGSearchbox extends BaseSearchbox {
 			return;
 
 		return (
-			<ul>
-				{ games.map(game => <li key={ game.id }>{ game.name }</li>) }
+			<ul className="dropdown-content select-dropdown active"
+			    style={ styles.autodropdown }>
+				{ games.map( this.renderSuggestion.bind(this) ) }
 			</ul>
 		);
 	}
 
 	render() {
 		return (
-			<div>
+			<div style={{position: "relative"}}>
 				<Input 
 					s={9}
 					label="Titre"
