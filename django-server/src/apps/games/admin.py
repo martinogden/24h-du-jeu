df3dcd0fa642bea5b4a20bd1c8422659732cd05f
Celine Aussourd <celine.aussourd@gmail.com>
2016-12-14 17:46:42 +0000

    add many to many fields

--- django-server/src/apps/games/admin.py
+++ django-server/src/apps/games/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Game, Knower, Owner, User
 
+class OwnerInline(admin.TabularInline):
+	model = Owner
+	extra = 1 # how many rows to show
+
+class KnowerInline(admin.TabularInline):
+	model = Knower
+	extra = 1 # how many rows to show
+
+class GameAdmin(admin.ModelAdmin):
+	inlines = (OwnerInline, KnowerInline, )
+	list_display = ('name',)
+	list_filter = ('name',)
+	search_fields = ('name','id_bgg')
+
+class UserAdmin(admin.ModelAdmin):
+	inlines = (OwnerInline, KnowerInline,)
+	list_display = ('pseudo',)
+
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ('fk_player', 'fk_game')
     # raise TypeError('Related Field got invalid lookup: %s' % lookup_name)
@@ -13,8 +31,9 @@ class KnowerAdmin(admin.ModelAdmin):
     # search_fields = ('fk_game')
     list_filter = ('fk_player', 'fk_game')
     # for the ordering the admin is using the ID and not the name
+		
 
-admin.site.register(Game)
-admin.site.register(User)
+admin.site.register(Game, GameAdmin)
+admin.site.register(User, UserAdmin)
 admin.site.register(Knower, KnowerAdmin)
 admin.site.register(Owner, OwnerAdmin)
