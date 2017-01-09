da2fb22303842d98258ccbf6af158621a8a68ea7
Celine Aussourd <celine.aussourd@gmail.com>
2017-01-09 17:52:05 +0000

    separate the pdfs

--- django-server/src/apps/games/urls.py
+++ django-server/src/apps/games/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from . import views
+from . import views, pdfs
 
 urlpatterns = [
 	url(r'^bgg/$', views.bgg_games, name='bgg-games'),
 
-	url(r'^pdf/recap/$', views.pdf_recap, name='pdf-recap'),
-	url(r'^pdf/jeu_genre/$', views.pdf_jeu_genre, name='pdf-jeu-genre'),
-	url(r'^pdf/par_genre/$', views.pdf_par_genre, name='pdf-par-genre'),
+	url(r'^pdf/recap/$', pdfs.pdf_recap, name='pdf-recap'),
+	url(r'^pdf/jeu_genre/$', pdfs.pdf_jeu_genre, name='pdf-jeu-genre'),
+	url(r'^pdf/par_genre/$', pdfs.pdf_par_genre, name='pdf-par-genre'),
 
 	url(r'(?P<game_id>\d+)/owners/$', views.owners, name='owners'),
 	url(r'(?P<game_id>\d+)/knowers/$', views.knowers, name='knowers'),
