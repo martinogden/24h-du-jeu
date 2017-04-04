8e06cf49f14a22e91abaffb660a44fefe9ab6f6a
Céline Aussourd <celine.aussourd@gmail.com>
2017-04-04 22:38:37 +0100

    add values to fields

--- client/src/components/AddGame.js
+++ client/src/components/AddGame.js
@@ -23,24 +23,24 @@ const AddGame = ({ onSearch, autocomplete, onSelect, bggGame }) => (
 			}>
 				<Row>
 					<BGGSearchbox id="name" search={ onSearch } select={ onSelect } autocomplete={ autocomplete }/>
-					<Input id="id_bgg" s={3} label="ID BoardGameGeek" defaultValue="" disabled />
+					<Input id="id_bgg" s={3} label="ID BoardGameGeek" defaultValue="" value={ bggGame.id } disabled />
 					<Input id="type_genre" s={12} m={4} type='select' label="Genre">
-					    <option value="" disabled selected>Sélectionner le genre</option>
-					    <option value='1'>Ambiance</option>
-					    <option value='2'>Coopératif</option>
-					    <option value='3'>Enchères</option>
-					    <option value='4'>Enfants</option>
-					    <option value='5'>Gestion</option>
-					    <option value='6'>Parcours</option>
-					    <option value='7'>Placement</option>
-					    <option value='8'>Stratégie</option>
+					    <option value="" disabled selected={ bggGame.type_genre || 'selected' }>Sélectionner le genre</option>
+					    <option value='Ambiance' selected={bggGame.type_genre == 'Ambiance' ? 'selected' : '' }>Ambiance</option>
+					    <option value='Coopératif' selected={bggGame.type_genre == 'Coopératif' ? 'selected' : '' }>Coopératif</option>
+					    <option value='Enchères' selected={bggGame.type_genre == 'Enchères' ? 'selected' : '' }>Enchères</option>
+					    <option value='Enfants' selected={bggGame.type_genre == 'Enfants' ? 'selected' : '' }>Enfants</option>
+					    <option value='Gestion' selected={bggGame.type_genre == 'Gestion' ? 'selected' : '' }>Gestion</option>
+					    <option value='Parcours' selected={bggGame.type_genre == 'Parcours' ? 'selected' : '' }>Parcours</option>
+					    <option value='Placement' selected={bggGame.type_genre == 'Placement' ? 'selected' : '' }>Placement</option>
+					    <option value='Stratégie' selected={bggGame.type_genre == 'Stratégie' ? 'selected' : '' }>Stratégie</option>
 					</Input>
 					<Input id="min_player" s={6} m={2} label="Joueurs min." value={ bggGame.min_player }/>
-					<Input id="max_player" s={6} m={2} label="Joueurs max." />
-					<Input id="duration" s={6} m={2} label="Durée (mins)" />
-					<Input id="min_age" s={6} m={2} label="Age min." />
+					<Input id="max_player" s={6} m={2} label="Joueurs max." value={ bggGame.max_player }/>
+					<Input id="duration" s={6} m={2} label="Durée (mins)" value={ bggGame.duration }/>
+					<Input id="min_age" s={6} m={2} label="Age min." value={ bggGame.min_age }/>
 					<div className="input-field col s12">
-						<textarea id="description" className="materialize-textarea"></textarea>
+						<textarea id="description" className="materialize-textarea" value={ bggGame.description }></textarea>
 	          			<label for="description">Description</label>
           			</div>
 				</Row>
