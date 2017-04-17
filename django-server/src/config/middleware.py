0844996f609d3b35190665c84a1b4bfb84654b64
Céline Aussourd <celine.aussourd@gmail.com>
2017-04-17 23:29:34 +0100

    hack to make admin interface work in dev

--- django-server/src/config/middleware.py
+++ django-server/src/config/middleware.py
@@ -4,6 +4,6 @@ from django.conf import settings
 class DevMiddleware(object):
 
 	def process_view(self, request, view_func, view_args, view_kwargs):
-		if settings.DEBUG:
+		if settings.DEBUG and not hasattr(view_func, 'im_self'):
 			view_func.csrf_exempt = True
 		return view_func(request, *view_args, **view_kwargs)
