2934e0d41b496a8c0ea849a5c7f7e92cef288136
Céline Aussourd <celine.aussourd@gmail.com>
2017-04-30 21:21:19 +0100

    Add dropdown menu

--- client/src/components/Sort.js
+++ client/src/components/Sort.js
@@ -1,14 +1,33 @@
 import React, { PropTypes } from 'react';
-import { NavItem, Icon } from 'react-materialize';
+import { NavItem, Icon, Dropdown } from 'react-materialize';
 
 
 const Sort = ({ onSortAlpha, onFetchGames, isSortedAlpha }) => {
-	if (isSortedAlpha) {
-		return (<NavItem href='#' onClick={ onFetchGames }><Icon>fiber_new</Icon></NavItem>);
-	}
-	else {	
-		return(<NavItem href='#' onClick={ onSortAlpha }><Icon>sort_by_alpha</Icon></NavItem>);
-	}
+	const getNavItem = () => {
+		if (isSortedAlpha) {
+			return (<NavItem href='#' onClick={ onFetchGames }>Par date d'ajout</NavItem>);
+		}
+		else {	
+			return(<NavItem href='#' onClick={ onSortAlpha }>Par ordre alphabétique</NavItem>);
+		}
+	};
+
+	return(
+		<li>
+			<Dropdown 
+				trigger={
+					<a href='#'><Icon>sort_by_alpha</Icon></a>
+				}
+				options={
+					{
+						constrain_width: false,
+						belowOrigin: true,
+					}	
+				}>
+				{ getNavItem() }
+			</Dropdown>
+		</li>
+	);
 		
 };
 
