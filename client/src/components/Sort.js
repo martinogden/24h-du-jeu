095e811b97b9f6ac370542f27cea5de0ffae4ed0
Céline Aussourd <celine.aussourd@gmail.com>
2017-05-06 17:45:23 +0100

    reorganise icons and text in menu

--- client/src/components/Sort.js
+++ client/src/components/Sort.js
@@ -5,17 +5,17 @@ import { NavItem, Icon } from 'react-materialize';
 const Sort = ({ onSortAlpha, onFetchGames, isSortedAlpha, isFiltered }) => {
 	if (!isFiltered) {
 		if (isSortedAlpha) {
-			return (<NavItem href='#' onClick={ onFetchGames }><Icon className='left'>sort_by_alpha</Icon>Par date d'ajout</NavItem>);
+			return (<NavItem href='#' onClick={ onFetchGames }><Icon className='left'>sort_by_alpha</Icon>Date d'ajout</NavItem>);
 		}
 		else {	
-			return(<NavItem href='#' onClick={ onSortAlpha }><Icon className='left'>sort_by_alpha</Icon>Par ordre alphabétique</NavItem>);
+			return(<NavItem href='#' onClick={ onSortAlpha }><Icon className='left'>sort_by_alpha</Icon>Ordre alphabétique</NavItem>);
 		}
 	}
 	else if (isSortedAlpha) {
-		return (<NavItem href='#' onClick={ onSortAlpha }><Icon className='left'>list</Icon>Tous les jeux</NavItem>);
+		return (<NavItem href='#' onClick={ onSortAlpha }><Icon className='left'>reorder</Icon>Tous les jeux</NavItem>);
 	}
 	else {
-		return(<NavItem href='#' onClick={ onFetchGames }><Icon className='left'>list</Icon>Tous les jeux</NavItem>);
+		return(<NavItem href='#' onClick={ onFetchGames }><Icon className='left'>reorder</Icon>Tous les jeux</NavItem>);
 	}
 		
 };
