25329b29a7fbf2f6756df3f22c0ddc90e789908d
Céline Aussourd <celine.aussourd@gmail.com>
2019-03-23 16:02:28 +0000

    Use French timezone (admins can now add/delete shifts)

--- django-server/src/config/settings.py
+++ django-server/src/config/settings.py
@@ -228,13 +228,13 @@ AUTHENTICATION_BACKENDS = (
 
 LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
