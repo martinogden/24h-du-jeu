4a87823b444f160c4b55e44b6524c18a6b96729b
Céline Aussourd <celine.aussourd@gmail.com>
2022-06-27 10:18:33 +0100

    remove old data deletion page

--- django-server/src/config/urls.py
+++ django-server/src/config/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^api/games/', include('games.urls')),
 	url(r'^$', TemplateView.as_view(template_name='index.html')),
-	url(r'^fb_user_data_deletion/', TemplateView.as_view(template_name='fb_user_data_deletion.html')),
 	url(r'^privacy_policy/', TemplateView.as_view(template_name='privacy_policy.html')),
 ]
 
